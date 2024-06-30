8a83a3f65b49031d164a30f2ca6aa243d5e3eb2e
d2vova <vprikhodko85@gmail.com>
2024-07-01 01:36:18 +0300

    add category

--- src/catalogue/services.py
+++ src/catalogue/services.py
@@ -1,10 +1,12 @@
+from typing import List, Optional
 from fastapi import Depends
 
-from src.catalogue.models.pydantic import ProductModel
+from src.catalogue.models.pydantic import ProductModel, CategoryModel
 from src.catalogue.repository import (
     ProductRepository,
-    get_product_repository,
+    get_product_repository, CategoryRepository, get_category_repository,
 )
+from src.common.exceptions.base import ObjectDoesNotExistException
 from src.common.service import BaseService
 
 
@@ -15,3 +17,30 @@ class ProductService(BaseService[ProductModel]):
 
 def get_product_service(repo: ProductRepository = Depends(get_product_repository)) -> ProductService:
     return ProductService(repository=repo)
+
+
+class CategoryService:
+    def __init__(self, repository: CategoryRepository):
+        self.repository = repository
+
+    async def get_all_categories(self) -> List[CategoryModel]:
+        return await self.repository.all()
+
+    async def get_category(self, category_id: int) -> Optional[CategoryModel]:
+        category = await self.repository.get(category_id)
+        if not category:
+            raise ObjectDoesNotExistException(f"Category with id {category_id} does not exist")
+        return category
+
+    async def create_category(self, category: CategoryModel) -> CategoryModel:
+        return await self.repository.create(category)
+
+    async def update_category(self, category_id: int, category: CategoryModel) -> Optional[CategoryModel]:
+        return await self.repository.update(category_id, category)
+
+    async def delete_category(self, category_id: int) -> bool:
+        return await self.repository.delete(category_id)
+
+
+def get_category_service(repository: CategoryRepository = Depends(get_category_repository)) -> CategoryService:
+    return CategoryService(repository)
